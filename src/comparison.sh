cfde326d30f77df32c136fa31530de231a02aab9
Thiago Guerrero <tgbalera@hotmail.com>
2021-07-29 21:34:34 -0300

    change comparison script
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

--- src/comparison.sh
+++ src/comparison.sh
@@ -2,13 +2,14 @@
 
 set -o xtrace
 
-make
 MEASUREMENTS=15
 MPI_SEQ_PROCS=16
 MPI_THRD_PROCS=4
-THREADS=8
-
+MPI_THREADS=8
+EP1_THREADS=32 
 MPI_FLAG=0
+
+make
 
 NAMES=('mandelbrot_seq' 'mandelbrot_pth' 'mandelbrot_omp' 'mandelbrot_mpi' 'mandelbrot_mpi_pth' 'mandelbrot_mpi_omp')
 mkdir -p ../measurements
@@ -24,12 +25,12 @@ for NAME in ${NAMES[@]}; do
 
     elif [ $MPI_FLAG -eq 1 ] ; then
         for k in $(seq $MEASUREMENTS); do
-            mpirun -np $MPI_THRD_PROCS $NAME $THREADS >> ../measurements/comparison.csv
+            mpirun -np $MPI_THRD_PROCS $NAME $MPI_THREADS >> ../measurements/comparison.csv
         done
 
     else
         for k in $(seq $MEASUREMENTS); do
-            ./$NAME $THREADS >> ../measurements/comparison.csv
+            ./$NAME $EP1_THREADS >> ../measurements/comparison.csv
         done
     fi
 
